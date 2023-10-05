4db2298ae944d8fd2cbd06b97cce59e9c05ba76a
Liwen <liwenweng2007@gmail.com>
2023-10-04 21:32:18 -0400

    terminal compile script

---
+++ src/compile.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Get the current directory
+CURRENT_DIR=$(dirname "${BASH_SOURCE[0]}")
+
+# Navigate to the script's directory
+cd "$CURRENT_DIR" || exit
+
+# Compile all Java files in the directory
+for file in *.java; do
+    if [ -f "$file" ]; then
+        javac "$file"
+        # Check if the compilation was successful
+        if [ $? -eq 0 ]; then
+            echo "Compilation of $file successful."
+        else
+            echo "Compilation of $file failed."
+        fi
+    fi
+done
